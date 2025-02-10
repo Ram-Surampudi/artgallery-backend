b48c23a28894e6a1d3a90c23486b47197cca86c1
Ram-Surampudi <155422559+Ram-Surampudi@users.noreply.github.com>
2025-02-10 07:52:03 +0530

    Update ArtController.java

--- src/main/java/com/example/demo/Controllers/ArtController.java
+++ src/main/java/com/example/demo/Controllers/ArtController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://online-art-gallery-frontend-jfsd.vercel.app/")
 @RestController
 @RequestMapping("/art")
 public class ArtController {
